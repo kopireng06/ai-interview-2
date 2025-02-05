a4e54ca666fdba3e46c474cb13deb06c7537587b
Akbar Izza Mahendra Suni <akbarizza09@gmail.com>
2025-02-05 16:58:40 +0700

    asds

--- src/network/const.ts
+++ src/network/const.ts
@@ -11,29 +11,29 @@ export const questions = [
     id: 3,
     text: "Bagaimana Anda menangani situasi ketika ada perubahan mendadak dalam proyek atau prioritas kerja?",
   },
-  {
-    id: 4,
-    text: "Apa yang Anda cari dalam sebuah perusahaan selain gaji dan benefit?",
-  },
-  {
-    id: 5,
-    text: "Apa yang Anda ketahui tentang budaya perusahaan kami, dan bagaimana Anda melihat diri Anda cocok dengan budaya tersebut?",
-  },
-  { id: 6, text: "Bagaimana Anda menangani perbedaan pendapat dalam tim?" },
-  {
-    id: 7,
-    text: "Bagaimana Anda merespons feedback dari atasan atau rekan kerja?",
-  },
-  {
-    id: 8,
-    text: "Apa tantangan terbesar yang Anda antisipasi dalam peran ini, dan bagaimana Anda berencana untuk mengatasinya?",
-  },
-  {
-    id: 9,
-    text: "Ceritakan pengalaman Anda dalam menyelesaikan masalah yang mirip dengan tanggung jawab di posisi ini",
-  },
-  {
-    id: 10,
-    text: "Bagaimana Anda melihat perkembangan karier Anda dalam 2-3 tahun ke depan?",
-  },
+  // {
+  //   id: 4,
+  //   text: "Apa yang Anda cari dalam sebuah perusahaan selain gaji dan benefit?",
+  // },
+  // {
+  //   id: 5,
+  //   text: "Apa yang Anda ketahui tentang budaya perusahaan kami, dan bagaimana Anda melihat diri Anda cocok dengan budaya tersebut?",
+  // },
+  // { id: 6, text: "Bagaimana Anda menangani perbedaan pendapat dalam tim?" },
+  // {
+  //   id: 7,
+  //   text: "Bagaimana Anda merespons feedback dari atasan atau rekan kerja?",
+  // },
+  // {
+  //   id: 8,
+  //   text: "Apa tantangan terbesar yang Anda antisipasi dalam peran ini, dan bagaimana Anda berencana untuk mengatasinya?",
+  // },
+  // {
+  //   id: 9,
+  //   text: "Ceritakan pengalaman Anda dalam menyelesaikan masalah yang mirip dengan tanggung jawab di posisi ini",
+  // },
+  // {
+  //   id: 10,
+  //   text: "Bagaimana Anda melihat perkembangan karier Anda dalam 2-3 tahun ke depan?",
+  // },
 ];
