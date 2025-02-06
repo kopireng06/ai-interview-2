45cf53527dbd52f01fc12d4569c909a9c441bd47
Akbar Izza Mahendra Suni <akbarizza09@gmail.com>
2025-02-06 09:50:21 +0700

    hi

--- src/network/const.ts
+++ src/network/const.ts
@@ -1,39 +1,22 @@
 export const questions = [
   {
     id: 1,
-    text: "Apa komitmen Anda terhadap pekerjaan dan bagaimana Anda menunjukkan komitmen tersebut dalam tugas sehari-hari?",
+    text: "Apa yang membuat Anda bertahan di perusahaan sebelumnya dalam jangka waktu yang lama?",
   },
   {
     id: 2,
-    text: "Ceritakan pengalaman Anda saat harus mempelajari hal baru atau bekerja di lingkungan yang berbeda dari biasanya?",
-  },
-  {
-    id: 3,
     text: "Bagaimana Anda menangani situasi ketika ada perubahan mendadak dalam proyek atau prioritas kerja?",
   },
   {
-    id: 4,
-    text: "Apa yang Anda cari dalam sebuah perusahaan selain gaji dan benefit?",
+    id: 3,
+    text: "Bagaimana Anda menangani perbedaan pendapat dalam tim?",
   },
-  // {
-  //   id: 5,
-  //   text: "Apa yang Anda ketahui tentang budaya perusahaan kami, dan bagaimana Anda melihat diri Anda cocok dengan budaya tersebut?",
-  // },
-  // { id: 6, text: "Bagaimana Anda menangani perbedaan pendapat dalam tim?" },
-  // {
-  //   id: 7,
-  //   text: "Bagaimana Anda merespons feedback dari atasan atau rekan kerja?",
-  // },
-  // {
-  //   id: 8,
-  //   text: "Apa tantangan terbesar yang Anda antisipasi dalam peran ini, dan bagaimana Anda berencana untuk mengatasinya?",
-  // },
-  // {
-  //   id: 9,
-  //   text: "Ceritakan pengalaman Anda dalam menyelesaikan masalah yang mirip dengan tanggung jawab di posisi ini",
-  // },
-  // {
-  //   id: 10,
-  //   text: "Bagaimana Anda melihat perkembangan karier Anda dalam 2-3 tahun ke depan?",
-  // },
+  {
+    id: 4,
+    text: "Apa tantangan terbesar yang Anda antisipasi dalam peran ini, dan bagaimana Anda berencana untuk mengatasinya?",
+  },
+  {
+    id: 5,
+    text: "Ceritakan pengalaman Anda saat harus bekerja dengan orang yang memiliki kepribadian sangat berbeda dengan Anda.",
+  },
 ];
