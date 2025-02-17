8ab453859b8a6d90882f94673e3ed030f2c508fd
Akbar Izza Mahendra Suni <akbarizza09@gmail.com>
2025-02-17 14:51:06 +0700

    update

--- src/const.ts
+++ src/const.ts
@@ -1,7 +1,7 @@
 export const questions = [
   {
     id: 1,
-    text: "Ceritakan tentang suatu situasi di mana Anda harus menyampaikan informasi penting kepada orang lain. Apa tujuan komunikasi Anda? Bagaimana Anda menyusun pesan Anda? Bagaimana reaksi penerima? Apa hasil akhirnya?",
+    text: "Ceritakan tentang suatu situasi di mana Anda harus menyampaikan informasi penting kepada orang lain. Bagaimana cara yang Anda lakukan untuk dapat menyampaikan pesan tersebut dengan baik?",
   },
   {
     id: 2,
@@ -9,14 +9,14 @@ export const questions = [
   },
   {
     id: 3,
-    text: "Ceritakan tentang suatu situasi di mana Anda harus bertanggung jawab atas suatu tugas yang sulit atau menantang. Apa tugasnya, dan mengapa itu menantang? Bagaimana Anda menangani situasi tersebut? Apa hasil akhirnya?",
+    text: "Ceritakan tentang suatu situasi di mana Anda harus bertanggung jawab atas suatu tugas yang sulit atau menantang. Bagaimana Anda menangani situasi tersebut?",
   },
   {
     id: 4,
-    text: "Pernahkah Anda membantu seseorang yang sedang mengalami kesulitan atau frustasi? Bagaimana Anda mengenali kebutuhan mereka? Apa dampak dari tindakan Anda?",
+    text: "Pernahkah Anda membantu seseorang yang sedang mengalami kesulitan atau frustrasi? Bagaimana Anda mengenali kebutuhan mereka dan bantuan apa yang diberikan?",
   },
   {
     id: 5,
-    text: "Ceritakan tentang pengalaman Anda saat menghadapi hambatan besar dalam mencapai tujuan. Apa tantangan atau hambatan yang Anda hadapi? Bagaimana Anda mengatasinya? Apa dampaknya terhadap hasil akhir?",
+    text: "Ceritakan tentang pengalaman Anda saat menghadapi hambatan besar dalam mencapai tujuan. Bagaimana Anda mengatasinya?",
   },
 ];
